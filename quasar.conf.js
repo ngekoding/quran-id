151d22136846dd705d294b23f891df3e4742bb47
Nur Muhammad <blog.nurmuhammad@gmail.com>
2021-02-03 18:01:09 +0800

    Prepare for deploy

--- quasar.conf.js
+++ quasar.conf.js
@@ -7,7 +7,7 @@
 // https://quasar.dev/quasar-cli/quasar-conf-js
 /* eslint-env node */
 
-module.exports = function(/* ctx */) {
+module.exports = function(ctx) {
   return {
     // https://quasar.dev/quasar-cli/supporting-ts
     supportTS: false,
@@ -40,7 +40,7 @@ module.exports = function(/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: "hash", // available values: 'hash', 'history'
-
+      publicPath: ctx.dev ? "/" : "/quran-id/",
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
