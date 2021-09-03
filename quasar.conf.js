9c7d32b75a13d7d4ce0654c738c19016cb491e65
Nur Muhammad <blog.nurmuhammad@gmail.com>
2021-09-03 08:38:13 +0800

    Fix quasar config (forget to uncomment)

--- quasar.conf.js
+++ quasar.conf.js
@@ -40,7 +40,7 @@ module.exports = function(ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: "history", // available values: 'hash', 'history'
-      // publicPath: ctx.dev ? "/" : "/quran-id/",
+      publicPath: ctx.dev ? "/" : "/quran-id/",
       // transpile: false,
       env: {
         QURAN_API_BASE_URL: "https://api.quran.com/api/v4/",
