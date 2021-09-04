8b80f863bdd21d102b868d66662bfecfbd6d8897
Nur Muhammad <blog.nurmuhammad@gmail.com>
2021-09-04 21:32:44 +0800

    Improve arabic normalize text helper

--- src/lib/search-helper.js
+++ src/lib/search-helper.js
@@ -5,23 +5,12 @@
  * Soruce: https://stackoverflow.com/a/40959537/6829171
  */
 const normalizeText = function(text) {
-  //remove special characters
-  text = text.replace(
-    /([^\u0621-\u063A\u0641-\u064A\u0660-\u0669a-zA-Z 0-9])/g,
-    ""
-  );
-
-  //normalize Arabic
-  text = text.replace(/(آ|إ|أ)/g, "ا");
-  text = text.replace(/(ة)/g, "ه");
+  text = text.replace(/(آ|إ|أ|ٱ|ٲ|ٳ)/g, "ا");
   text = text.replace(/(ئ|ؤ)/g, "ء");
   text = text.replace(/(ى)/g, "ي");
 
-  //convert arabic numerals to english counterparts.
-  const starter = 0x660;
-  for (let i = 0; i < 10; i++) {
-    text.replace(String.fromCharCode(starter + i), String.fromCharCode(48 + i));
-  }
+  // Remove special characters
+  text = text.replace(/([^\u0621-\u063A\u0641-\u064A\u0660-\u0669])/g, "");
 
   return text;
 };
