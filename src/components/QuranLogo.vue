9f229ad0264518f6cb687d32cc056c8574488ec4
Nur Muhammad <blog.nurmuhammad@gmail.com>
2024-12-28 06:38:02 +0800

    Change app short url to bit.ly/quran-id

--- src/components/QuranLogo.vue
+++ src/components/QuranLogo.vue
@@ -2,7 +2,7 @@
   <div class="quran-logo column items-center q-my-lg">
     <img src="~assets/images/Quran-Kareem.svg" width="100px" />
     <div class="text-h6 q-mt-sm">Al-Quran dan Terjemahan</div>
-    <div class="text-caption">s.id/quran-id</div>
+    <div class="text-caption">bit.ly/quran-id</div>
   </div>
 </template>
 
