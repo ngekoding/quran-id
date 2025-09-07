1e957382f01f135866fbb9bde27f0f6162d1522a
Nur Muhammad <blog.nurmuhammad@gmail.com>
2025-09-07 15:18:22 +0800

    Fix for quran.com api changes

--- quasar.conf.js
+++ quasar.conf.js
@@ -7,7 +7,7 @@
 // https://quasar.dev/quasar-cli/quasar-conf-js
 /* eslint-env node */
 
-module.exports = function (ctx) {
+module.exports = function(ctx) {
   return {
     // https://quasar.dev/quasar-cli/supporting-ts
     supportTS: false,
@@ -34,7 +34,7 @@ module.exports = function (ctx) {
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
       "roboto-font", // optional, you are not bound to it
-      "material-icons", // optional, you are not bound to it
+      "material-icons" // optional, you are not bound to it
     ],
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
@@ -43,8 +43,10 @@ module.exports = function (ctx) {
       publicPath: ctx.dev ? "/" : process.env.PUBLIC_PATH ?? "/quran-id/",
       // transpile: false,
       env: {
-        QURAN_API_BASE_URL: "https://api.quran.com/api/v4/",
-        GTAG_ENABLED: ctx.prod,
+        QURAN_API_BASE_URL: ctx.dev
+          ? "http://localhost:3000/"
+          : "https://quran-com-api-proxy.vercel.app/",
+        GTAG_ENABLED: ctx.prod
       },
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
@@ -67,16 +69,16 @@ module.exports = function (ctx) {
           enforce: "pre",
           test: /\.(js|vue)$/,
           loader: "eslint-loader",
-          exclude: /node_modules/,
+          exclude: /node_modules/
         });
-      },
+      }
     },
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
       port: 8080,
-      open: true, // opens browser window automatically
+      open: true // opens browser window automatically
     },
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
@@ -98,7 +100,7 @@ module.exports = function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: ["Notify", "LocalStorage", "Meta"],
+      plugins: ["Notify", "LocalStorage", "Meta"]
     },
 
     // animations: 'all', // --- includes all animations
@@ -107,7 +109,7 @@ module.exports = function (ctx) {
 
     // https://quasar.dev/quasar-cli/developing-ssr/configuring-ssr
     ssr: {
-      pwa: false,
+      pwa: false
     },
 
     // https://quasar.dev/quasar-cli/developing-pwa/configuring-pwa
@@ -115,7 +117,7 @@ module.exports = function (ctx) {
       workboxPluginMode: "GenerateSW", // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {
         skipWaiting: true,
-        clientsClaim: true,
+        clientsClaim: true
       }, // only for GenerateSW
       manifest: {
         name: `Al-Quran`,
@@ -129,30 +131,30 @@ module.exports = function (ctx) {
           {
             src: "icons/icon-128x128.png",
             sizes: "128x128",
-            type: "image/png",
+            type: "image/png"
           },
           {
             src: "icons/icon-192x192.png",
             sizes: "192x192",
-            type: "image/png",
+            type: "image/png"
           },
           {
             src: "icons/icon-256x256.png",
             sizes: "256x256",
-            type: "image/png",
+            type: "image/png"
           },
           {
             src: "icons/icon-384x384.png",
             sizes: "384x384",
-            type: "image/png",
+            type: "image/png"
           },
           {
             src: "icons/icon-512x512.png",
             sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
+            type: "image/png"
+          }
+        ]
+      }
     },
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-cordova-apps/configuring-cordova
@@ -162,7 +164,7 @@ module.exports = function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-capacitor-apps/configuring-capacitor
     capacitor: {
-      hideSplashscreen: true,
+      hideSplashscreen: true
     },
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-electron-apps/configuring-electron
@@ -183,7 +185,7 @@ module.exports = function (ctx) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: "quran-id",
+        appId: "quran-id"
       },
 
       // More info: https://quasar.dev/quasar-cli/developing-electron-apps/node-integration
@@ -192,7 +194,7 @@ module.exports = function (ctx) {
       extendWebpack(/* cfg */) {
         // do something with Electron main process Webpack cfg
         // chainWebpack also available besides this extendWebpack
-      },
-    },
+      }
+    }
   };
 };
